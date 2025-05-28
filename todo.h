1ee1da8de6f3a63a5518ec45662154ec1699fb6b
soru <ityouna007@gmail.com>
2025-05-28 21:10:32 +0900

    関数宣言

--- todo.h
+++ todo.h
@@ -4,4 +4,14 @@
 #define MAX_TASKS 100
 #define TITLE_LENGTH 100
 
+typedef struct 
+{
+  int id;
+  char title[TITLE_LENGTH];
+  int completed;
+}Task;
+
+void add_task(Task tasks[], int *count);
+void list_tasks(Task tasks[], int count);
+
 #endif
