cfca752509515ccd1980beb95fc3dc94c4c0a21f
soru <ityouna007@gmail.com>
2025-05-31 23:46:22 +0900

    起動時のロードを

--- main.c
+++ main.c
@@ -6,4 +6,10 @@ int main() {
     Task tasks[MAX_TASKS];
     int task_count = 0;
     int choice;
+
+    // 起動時にタスクをロード
+    task_count = load_tasks(tasks);
+    printf("TODOアプリケーションへようこそ！\n");
+    printf("%d個のタスクをロードしました。\n\n", task_count);
+
 }
